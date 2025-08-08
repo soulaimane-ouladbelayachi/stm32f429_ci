19c9ce08289c128d01f560c65efdf98ba6e5f53d
Soulaimane Oulad Belayachi <soulaimane.ouladbelayachi@gmail.com>
2025-08-09 00:20:52 +0100

    add gpio driver

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,6 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f4xx.h"
+#include "gpio_driver.h"
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
@@ -69,33 +70,25 @@ int main(void)
 
   /* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
 
+  gpio_init_t led = {
+      .port        = GPIOG,
+      .pin         = LL_GPIO_PIN_13,
+      .mode        = GPIO_MODE_OUTPUT,
+      .output_type = GPIO_OUTPUT_PUSHPULL,
+      .speed       = GPIO_SPEED_HIGH,
+      .pull        = GPIO_NO_PULL,
+      .alternate   = 0
+  };
 
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
-
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
-
-  /* USER CODE BEGIN 2 */
-
-  /* USER CODE END 2 */
+  gpio_init(&led);
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    /* USER CODE END WHILE */
-    
-    /* USER CODE BEGIN 3 */
+    gpio_toggle(GPIOD, LL_GPIO_PIN_12);
+    for (volatile int i = 0; i < 1000000; i++);
   }
   /* USER CODE END 3 */
 }
